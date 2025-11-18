e0837da048ea062d5cc992dfc1b4487eceb53b4a
LanglangLian <langlang.lian@mail.utoronto.ca>
2025-11-18 10:53:55 -0500

    Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -28,25 +28,27 @@ public class StatementPrinter {
                                                         + invoice.getCustomer()
                                                         + System.lineSeparator());
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance performance : invoice.getPerformances()) {
 
             volumeCredits += getVolumeCredits(performance);
-
+            
             // print line for this order
             result.append(String.format("  %s: %s "
                                         + "(%s seats)%n",
                                         getPlay(performance).getName(),
-                                        frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR),
+                                        usd(getAmount(performance)),
                                         performance.getAudience()));
             totalAmount += getAmount(performance);
         }
         result.append(String.format("Amount owed is %s%n",
-                                    frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
+                usd(totalAmount)));
         result.append(String.format("You earned %s credits%n",
                                     volumeCredits));
         return result.toString();
+    }
+
+    private String usd(int totalAmount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / Constants.PERCENT_FACTOR);
     }
 
     private int getVolumeCredits(Performance performance) {
